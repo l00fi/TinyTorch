0bc4e0d0fccff1969715f7c9310c62ad682190c1
l00fi <yura.kabantsov@gmail.com>
2026-02-25 19:15:51 +0300

    auto-test... test

--- TinyTorch/TinyTorch.c
+++ TinyTorch/TinyTorch.c
@@ -1,44 +1,6 @@
-﻿#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-#include <string.h>
-#include "Vector.h"
+﻿#include "Vector.h"
 #include "Tensor.h"
-#include "Tests.h"
-#include "TinyTorch.h"
 
-main() {	
-	// ------------- Тесты -------------
-	printf("Starting TinyTorch Unit Tests...\n\n");
-
-	RUN_TEST(vector_emptyCorrect_init);
-	RUN_TEST(vector_Correct_init);
-	RUN_TEST(vector_Empty_arr);
-	RUN_TEST(vector_getPositive_index);
-	RUN_TEST(vector_getNegative_index);
-	RUN_TEST(vector_appendCorrect_works);
-	RUN_TEST(vector_editPositive_index);
-	RUN_TEST(vector_editNegative_index);
-	RUN_TEST(vector_mergeCorrect_works);
-	RUN_TEST(vector_mergeWith_zero_size_vector);
-	RUN_TEST(vector_mergeWith_zero_size_vectors);
-	RUN_TEST(vector_copyCorrect_works);
-	RUN_TEST(vector_sumCorrect_works_int);
-	RUN_TEST(vector_sumCorrect_works_float);
-	RUN_TEST(vector_sumCorrect_works_double);
-
-	RUN_TEST(tensor_Correct_init);
-	RUN_TEST(tensor_getCorrect_works);
-	RUN_TEST(tensor_sumCorrect_works_int);
-	RUN_TEST(tensor_sumCorrect_works_float);
-	RUN_TEST(tensor_sumCorrect_works_double);
-	RUN_TEST(tensor_TCorrect_works);
-	RUN_TEST(tensor_sumTTensor);
-	RUN_TEST(tensor_scalar_multCorrect_works);
-	RUN_TEST(tensor_hadamard_multCorrect_works);
-
-	print_test_report();
-	// ---------------------------------
-
-	return (global_state.failed_count > 0);
+main() {
+	return 0;
 }
